58b184f2d14513e57e797d2c72151b4675899e43
Carlos25050 <99521249+Carlos25050@users.noreply.github.com>
2022-12-15 22:06:11 -0500

    Secuencialeds
    Secuencia leds- digitales 2 unicauca

---
+++ build/default/production/main_pines_CJ.o.d
@@ -0,0 +1,3 @@
+build/default/production/main_pines_CJ.o:  \
+	C:/Users/Logicos/Documents/CJ_DIGITALES_ENSAN/pines_CJ.X/main_pines_CJ.asm  \
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=pines_CJ.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/pines_CJ.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pinescj.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pinescj.x.tar
